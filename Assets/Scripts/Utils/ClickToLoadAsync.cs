abbd83f90572ce75984d324fd8143fab3cbca12b
Willian Gomes de Oliveira <Willian Gomes de Oliveira>
2016-04-29 16:27:37 -0300

    Simple develop of start menu

---
+++ Assets/Scripts/Utils/ClickToLoadAsync.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class ClickToLoadAsync : MonoBehaviour {
+
+    public Slider loadingBar;
+    public GameObject loadingImage;
+
+    private AsyncOperation async;
+
+    public void ClickAsync(int level)
+    {
+        loadingImage.SetActive(true);
+        StartCoroutine(LoadLevelWithBar(level));
+    }
+
+    IEnumerator LoadLevelWithBar (int level)
+    {
+        //async = Application.LoadLevelAsync(level);
+        async = SceneManager.LoadSceneAsync(level);
+        while (!async.isDone)
+        {
+            loadingBar.value = async.progress;
+            yield return null;
+        }
+    }
+}
