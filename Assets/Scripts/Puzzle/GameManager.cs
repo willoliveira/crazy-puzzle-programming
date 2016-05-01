32540715625ad85b787b717791da47198794b8b0
Willian Gomes de Oliveira <Willian Gomes de Oliveira>
2016-05-01 13:48:24 -0300

    Indentificação das peças cortadas

--- Assets/Scripts/Puzzle/GameManager.cs
+++ Assets/Scripts/Puzzle/GameManager.cs
@@ -71,9 +71,6 @@ public class GameManager : MonoBehaviour
 
     private void MovePuzzle(int dirX, int dirY)
     {
-        //Debug.Log("square-" + (posBlank.x) + "-" + (posBlank.y ));
-        //Debug.Log("square-" + (posBlank.x - dirY) + "-" + (posBlank.y - dirX));
-        //return;
         //pega o game object
         Transform cacheGameObject;
         cacheGameObject = Board.Find("square-" + (posBlank.x - dirY) + "-" + (posBlank.y - dirX));
@@ -85,37 +82,37 @@ public class GameManager : MonoBehaviour
         //move
         cacheGameObject.position = new Vector3(posBlank.y * 2, (columns - 1 - posBlank.x) * 2, 0);
         cacheGameObject.name = "square-" + posBlank.x + "-" + posBlank.y;
-
+        //posição correta da peça
         Debug.Log(cacheGameObject.GetComponent<Square>().Row + "-" + cacheGameObject.GetComponent<Square>().Column);
-        //atualiza o x
+        //atualiza o x da peça vazia
         posBlank.x = posBlank.x - dirY;
         posBlank.y = posBlank.y - dirX;
     }
 
     private void CropImage()
     {        
-        for (int x = columns - 1; x > -1; x--)
+        //for (int x = columns - 1; x > -1; x--)
+        for (int x = 0; x < columns; x++)
         {
-            for (int y = columns - 1; y > - 1; y--)
+            //for (int y = columns - 1; y > - 1; y--)
+            for (int y = 0; y < columns; y++)
             {
                 //se for no lugar da ultima peça não pega
-                if (x == 0 && (columns - 1 - y) == 3) continue;
+                if ((columns - 1 - x) == 0 && y == 3) continue;
                 //Pega os pixels do recorte que quero fazer
-                Color[] pix = image.GetPixels(image.width - cropSize - (x * cropSize), image.height - cropSize - ((columns - 1 - y) * cropSize), cropSize, cropSize);
+                Color[] pix = image.GetPixels(image.width - cropSize - ((columns - 1 - x) * cropSize), image.height - cropSize - ((y) * cropSize), cropSize, cropSize);
                 //Cria uma textura com o recorte
                 Texture2D squareTexture2d = new Texture2D(cropSize, cropSize);
                 squareTexture2d.SetPixels(pix);
                 squareTexture2d.wrapMode = TextureWrapMode.Clamp;
-                squareTexture2d.Apply();
-                
-                //
+                squareTexture2d.Apply();                
+                //monta o struct
                 StructCrop structCrop;
                 structCrop.crop = squareTexture2d;
                 structCrop.row = y;
                 structCrop.column = x;
-
                 //adiciona no array o recorte
-                //arrCropImages.Add(squareTexture2d);
+                arrCropImages.Add(squareTexture2d);
                 listObjCropImages.Add(structCrop);
             }
         }
@@ -134,19 +131,20 @@ public class GameManager : MonoBehaviour
                 if (arrCropImages.Count == 0 || 
                     ((x * columns) + y) == (columns * columns) - 1)
                 {
+                    //ainda não usei
                     posBlank.x = x;
                     posBlank.y = y;
                     matrizBoard[x, y] = 0;
                     return;
                 }
+                //ainda não usei
                 matrizBoard[x, y] = 1;
-                //pega a imagem
-                int randomPosition = Random.Range(0, arrCropImages.Count - 1);
-                //Texture2D randomCropImage = arrCropImages[randomPosition];
-                StructCrop StructCropImage =  listObjCropImages[(x * columns) + y];
-                Texture2D randomCropImage = arrCropImages[(x * columns) + y];
+                //randomiza a imagem
+                int randomPosition = Random.Range(0, listObjCropImages.Count - 1);
+                StructCrop StructCropImage = listObjCropImages[randomPosition];
+                Texture2D randomCropImage = StructCropImage.crop;
                 //remove do array
-                //arrCropImages.Remove(randomCropImage);
+                listObjCropImages.Remove(StructCropImage);
                 //Seta a imagem como Sprite
                 squareSpriteRenderer = SquareGameObject.GetComponent<SpriteRenderer>();
                 squareSpriteRenderer.sprite = Sprite.Create(randomCropImage, new Rect(0, 0, randomCropImage.width, randomCropImage.height), new Vector2(0, 0), cropSize);
@@ -156,8 +154,8 @@ public class GameManager : MonoBehaviour
                 instance.name = "square-" + x + "-" + y;
 
                 //instance.name = "square-" + y + "-" + x;
-                instance.GetComponent<Square>().Row = y;
-                instance.GetComponent<Square>().Column = x;
+                instance.GetComponent<Square>().Row = StructCropImage.row;
+                instance.GetComponent<Square>().Column = StructCropImage.column;
 
                 //Debug.Log("square-" + y + "-" + x);
                 //Adiciona no Board
@@ -168,6 +166,4 @@ public class GameManager : MonoBehaviour
         }
     }
 
-
 }
-
