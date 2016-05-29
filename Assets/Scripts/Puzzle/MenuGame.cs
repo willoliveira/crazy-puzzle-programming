17855d4abf22c07cca7cf4d9cfccb53c7bff9fa6
Willian Gomes de Oliveira <willian.goliveira@outlook.com>
2016-05-29 12:34:21 -0300

    Implementando botao voltar no menu

--- Assets/Scripts/Puzzle/MenuGame.cs
+++ Assets/Scripts/Puzzle/MenuGame.cs
@@ -7,13 +7,28 @@ public class MenuGame : MonoBehaviour {
 	public GameObject ModeSelect;
 	public GameObject DifficultySelect;
 	//
+	public GameObject buttonBack;
+	//
 	public GameObject gameManagerObject;
 	private GameManager gameManager;
+	
+	private enum MenuScreen
+	{
+		MainMenu,
+		SelectMode,
+		DiffilcultyMode
+	}
+
+	private MenuScreen mMenuScreen;
 
 	// Use this for initialization
 	void Start () {
 		//
 		gameManager = gameManagerObject.GetComponent<GameManager>();
+		//
+		mMenuScreen = MenuScreen.MainMenu;
+		//
+		buttonBack.SetActive(false);
 		//
 		MainMenu.SetActive(true);
 		ModeSelect.SetActive(false);
@@ -24,6 +39,9 @@ public class MenuGame : MonoBehaviour {
 	{
 		MainMenu.SetActive(false);
 		ModeSelect.SetActive(true);
+		//
+		mMenuScreen = MenuScreen.SelectMode;
+		buttonBack.SetActive(true);
 	}
 
 	public void btSelectMode(int mode)
@@ -33,13 +51,38 @@ public class MenuGame : MonoBehaviour {
 
 		ModeSelect.SetActive(false);
 		DifficultySelect.SetActive(true);
+		//
+		mMenuScreen = MenuScreen.DiffilcultyMode;
+		buttonBack.SetActive(true);
 	}
 
 	public void btSelectDifficulty(int mode)
 	{
 		//atribui o modo de jogo selecionado ao GameManager
 		gameManager.mDiffilcultyMode = (DiffilcultyMode)mode;
-
+		//
 		DifficultySelect.SetActive(false);
 	}
+
+	public void btBack()
+	{
+		if (mMenuScreen == MenuScreen.SelectMode)
+		{
+			MainMenu.SetActive(true);
+			ModeSelect.SetActive(false);
+			DifficultySelect.SetActive(false);
+			//
+			mMenuScreen = MenuScreen.MainMenu;
+			buttonBack.SetActive(false);
+		}
+		else if (mMenuScreen == MenuScreen.DiffilcultyMode)
+		{
+			MainMenu.SetActive(false);
+			ModeSelect.SetActive(true);
+			DifficultySelect.SetActive(false);
+			//
+			mMenuScreen = MenuScreen.SelectMode;
+		}
+	}
+
 }
