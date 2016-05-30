c2bdc792f722c73b97fc9b9ebb03a78e4ddc8ee1
Willian Gomes de Oliveira <willian.goliveira@outlook.com>
2016-05-29 23:42:52 -0300

    Teste com animações

--- Assets/Scripts/Puzzle/MenuGame.cs
+++ Assets/Scripts/Puzzle/MenuGame.cs
@@ -20,21 +20,22 @@ public class MenuGame : MonoBehaviour {
 	}
 
 	private MenuScreen mMenuScreen;
-
-	// Use this for initialization
+	
 	void Start () {
-		//
+		//Manager do jogo
 		gameManager = gameManagerObject.GetComponent<GameManager>();
-		//
+		//Referencia de qual tela o usuario esta
 		mMenuScreen = MenuScreen.MainMenu;
-		//
+		//Seta o botao voltar como inativo no inicio
 		buttonBack.SetActive(false);
-		//
+		//Seta a tela Main como ativa no inicio
 		MainMenu.SetActive(true);
 		ModeSelect.SetActive(false);
 		DifficultySelect.SetActive(false);
 	}
-
+	/// <summary>
+	/// Botao jogar no menu principal
+	/// </summary>
 	public void btJogar()
 	{
 		MainMenu.SetActive(false);
@@ -43,7 +44,10 @@ public class MenuGame : MonoBehaviour {
 		mMenuScreen = MenuScreen.SelectMode;
 		buttonBack.SetActive(true);
 	}
-
+	/// <summary>
+	/// Botao de selecao do tipo de jogo
+	/// </summary>
+	/// <param name="mode"></param>
 	public void btSelectMode(int mode)
 	{
 		//atribui o modo de jogo selecionado ao GameManager
@@ -55,7 +59,10 @@ public class MenuGame : MonoBehaviour {
 		mMenuScreen = MenuScreen.DiffilcultyMode;
 		buttonBack.SetActive(true);
 	}
-
+	/// <summary>
+	/// Botao de selecao do nivel de dificuldade
+	/// </summary>
+	/// <param name="mode"></param>
 	public void btSelectDifficulty(int mode)
 	{
 		//atribui o modo de jogo selecionado ao GameManager
@@ -64,6 +71,9 @@ public class MenuGame : MonoBehaviour {
 		DifficultySelect.SetActive(false);
 	}
 
+	/// <summary>
+	/// Botao voltar do menu principal do jogo
+	/// </summary>
 	public void btBack()
 	{
 		if (mMenuScreen == MenuScreen.SelectMode)
@@ -84,5 +94,4 @@ public class MenuGame : MonoBehaviour {
 			mMenuScreen = MenuScreen.SelectMode;
 		}
 	}
-
 }
