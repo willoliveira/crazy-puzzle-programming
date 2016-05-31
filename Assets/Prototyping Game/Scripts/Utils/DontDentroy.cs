de31d620ee8cb4fe007eedb75e0cbd28a1af2d29
Willian Gomes Oliveira <willian.oliveira@mstech.com.br>
2016-05-31 17:57:16 -0300

    Mudando estrutura de pastas

---
+++ Assets/Prototyping Game/Scripts/Utils/DontDentroy.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace PrototypingGame
+{
+	public class DontDentroy : MonoBehaviour
+	{
+
+
+		// Use this for initialization
+		void Awake()
+		{
+			DontDestroyOnLoad(gameObject);
+		}
+
+	}
+}
