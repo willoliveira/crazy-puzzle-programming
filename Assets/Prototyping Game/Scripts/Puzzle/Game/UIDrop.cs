bb606a5943612da24184a7cb528e0be0669d20a3
Willian Gomes de Oliveira <willian.goliveira@outlook.com>
2016-06-09 14:35:30 -0300

    Adaptação do drag das pecas e construcao do Board para o Board na UI

---
+++ Assets/Prototyping Game/Scripts/Puzzle/Game/UIDrop.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System;
+
+public class UIDrop : MonoBehaviour, IDropHandler
+{
+
+	public GameObject item
+	{
+		get
+		{
+			if (transform.childCount > 0)
+			{
+				return transform.GetChild(0).gameObject;
+			}
+			return null;
+		}
+	}
+
+	public void OnDrop(PointerEventData eventData)
+	{
+		Debug.Log("UIDrop OnDrop");
+	}
+}
