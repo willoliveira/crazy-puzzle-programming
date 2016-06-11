aaaa05534496713ca6530b98474f6d949672cd4e
Willian Gomes de Oliveira <willian.goliveira@outlook.com>
2016-06-11 08:55:18 -0300

    montagem da cena

--- Assets/Prototyping Game/Scripts/Puzzle/Game/BoardResponsive.cs
+++ Assets/Prototyping Game/Scripts/Puzzle/Game/BoardResponsive.cs
@@ -31,17 +31,6 @@ public class BoardResponsive : MonoBehaviour {
 
 			ScreenWidth = Screen.width;
 			ScreenHeight = Screen.height;
-
-			//=======================================================
-
-			float height = Camera.main.orthographicSize * 2;
-			float width = height * Screen.width / Screen.height; // basically height * screen aspect ratio
-
-			Sprite s = spriteRenderer.sprite;
-			float unitWidth = s.textureRect.width / s.pixelsPerUnit;
-			float unitHeight = s.textureRect.height / s.pixelsPerUnit;
-
-			spriteRenderer.transform.localScale = new Vector3(width / unitWidth, height / unitHeight);
 		}
 	}
 
