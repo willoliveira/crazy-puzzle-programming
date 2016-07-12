cce4fa2292a42bca055b2ead225945fbb89dfe5e
Willian Gomes de Oliveira <willian.goliveira@outlook.com>
2016-07-12 17:59:18 -0300

    Plugins android

---
+++ Assets/Prototyping Game/Scripts/Utils/AdsManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using GoogleMobileAds.Api;
+
+
+public class AdsManager : MonoBehaviour {
+
+	string adUnitId;
+
+	// Use this for initialization
+	void Start () {
+
+		#if UNITY_ANDROID
+		adUnitId = "ca-app-pub-9314300016729396/3916609662";
+		StartBannerGoogle();
+		#endif
+	}
+
+	private void StartBannerGoogle() {
+		// Create a 320x50 banner at the top of the screen.
+		BannerView bannerView = new BannerView(adUnitId, AdSize.Banner, AdPosition.Bottom);
+		// Create an empty ad request.
+		AdRequest request = new AdRequest.Builder().Build();
+		// Load the banner with the request.
+		bannerView.LoadAd(request);
+	}
+
+}
