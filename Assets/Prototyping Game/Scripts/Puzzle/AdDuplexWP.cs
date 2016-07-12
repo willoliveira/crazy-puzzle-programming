d2a59988ff03714b4d93ea17c294fd87c1da01b6
Willian Gomes de Oliveira <willian.goliveira@outlook.com>
2016-07-11 21:42:12 -0300

    AdDuplex teste para Windows Phone

---
+++ Assets/Prototyping Game/Scripts/Puzzle/AdDuplexWP.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AdDuplexWP : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		StartCoroutine(StartAds());
+	}
+
+	private IEnumerator StartAds()
+	{
+		yield return new WaitForSeconds(5);
+
+		ShowAdControlTapped();
+	}
+
+	private void ShowAdControlTapped()
+	{
+		AdDuplexInterop.ShowAdControl();
+	}
+}
