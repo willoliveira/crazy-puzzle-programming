8da356c02d375f7a54f6b96674236ec0e5811cbc
Willian Gomes de Oliveira <willian.goliveira@outlook.com>
2016-07-13 10:09:13 -0300

    Começando a integrar o ads do android e wp no mesmo projeto

--- Assets/Prototyping Game/Scripts/Utils/AdsManager.cs
+++ Assets/Prototyping Game/Scripts/Utils/AdsManager.cs
@@ -1,25 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+using AdDuplexUnitySDK;
+
+#if UNITY_ANDROID
 using GoogleMobileAds.Api;
+#endif
 using System;
 
 public class AdsManager : MonoBehaviour {
 
+	public AdDuplexUnitySDK.Assets.Plugins.AdDuplexManagement mAdDuplexManagement;
 	string adUnitId;
-	public 
+	#if UNITY_ANDROID
 	BannerView bannerView;
+	#endif
 
 	// Use this for initialization
 	void Start () {
 
 #if UNITY_ANDROID
 		adUnitId = "ca-app-pub-2302915872676550/5582174420";
-		StartBannerGoogle();
 #endif
+#if UNITY_WSA_10_0
+		adUnitId = "ca-app-pub-2302915872676550/5582174420";
+#endif
+		StartBanner();
 	}
 
-	private void StartBannerGoogle() {
+	private void StartBanner() {
+#if UNITY_ANDROID
 		// Create a 320x50 banner at the top of the screen.
 		bannerView = new BannerView(adUnitId, AdSize.Banner, AdPosition.Bottom);
 		bannerView.OnAdLoaded += HandleAdLoaded;
@@ -27,12 +37,16 @@ public class AdsManager : MonoBehaviour {
 		AdRequest request = new AdRequest.Builder().Build();
 		// Load the banner with the request.
 		bannerView.LoadAd(request);
+#endif
+#if UNITY_WSA_10_0
+#endif
 	}
 
-
+#if UNITY_ANDROID
 	public void HandleAdLoaded(object sender, EventArgs args)
 	{
 		bannerView.Show();
 	}
+#endif
 
 }
