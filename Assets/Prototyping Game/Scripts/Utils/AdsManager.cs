b22e7a2fa8050ec53ce1562ba16eaaaaeab852fc
Willian Gomes de Oliveira <willian.goliveira@outlook.com>
2016-07-19 19:01:51 -0300

    mascara na selecao de modos

--- Assets/Prototyping Game/Scripts/Utils/AdsManager.cs
+++ Assets/Prototyping Game/Scripts/Utils/AdsManager.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System;
 using System.Collections;
-#if UNITY_ANDROID
+#if !UNITY_EDITOR && UNITY_ANDROID
 using GoogleMobileAds.Api;
 #endif
 
@@ -10,14 +10,14 @@ public class AdsManager : MonoBehaviour {
 #if UNITY_WSA_10_0
 	public GameObject mAdDuplexManagement;
 #endif
-#if UNITY_ANDROID
+#if !UNITY_EDITOR && UNITY_ANDROID
 	string adUnitId;
 	BannerView bannerView;
 #endif
 
 	// Use this for initialization
 	void Start () {
-#if UNITY_ANDROID
+#if !UNITY_EDITOR && UNITY_ANDROID
 		adUnitId = "ca-app-pub-2302915872676550/5582174420";
 		StartBanner();
 #endif
@@ -26,7 +26,7 @@ public class AdsManager : MonoBehaviour {
 #endif
 	}
 
-#if UNITY_ANDROID
+#if !UNITY_EDITOR && UNITY_ANDROID
 	private void StartBanner() {
 		// Create a 320x50 banner at the top of the screen.
 		bannerView = new BannerView(adUnitId, AdSize.Banner, AdPosition.Bottom);
