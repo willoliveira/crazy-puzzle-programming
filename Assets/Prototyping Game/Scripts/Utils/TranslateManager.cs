f589dfe1ad957138c929c7eebb922e0d324938de
Willian Gomes de Oliveira <willian.goliveira@outlook.com>
2016-07-22 21:01:02 -0300

    Teste com localização e inclusão do idioma ingles

---
+++ Assets/Prototyping Game/Scripts/Utils/TranslateManager.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Globalization;
+using SmartLocalization;
+
+public class TranslateManager : MonoBehaviour {
+
+	LanguageManager mLanguageManager;
+	public Text t;
+
+	private string returnLanguageCode()
+	{
+		string retorno = "";
+		switch (Application.systemLanguage + "")
+		{
+			case "English":
+				retorno = "en";
+				break;
+			case "French":
+				retorno = "fr";
+				break;
+			case "Germany":
+				retorno = "de";
+				break;
+			case "Korean":
+				retorno = "Ko";
+				break;
+			case "Portuguese":
+				retorno = "pt";
+				break;
+			case "Spanish":
+				retorno = "es";
+				break;
+			case "Italian":
+				retorno = "it";
+				break;
+		}
+
+		return retorno;
+	}
+
+	void Awake()
+	{
+		mLanguageManager = LanguageManager.Instance;
+
+		SetLanguageGame();
+	}
+
+	private void SetLanguageGame()
+	{
+		string strLanguageCode;
+		string systemLanguage = Application.systemLanguage + "";
+
+		t.text = Application.systemLanguage + ""; 
+		t.text = systemLanguage + " - " + returnLanguageCode();
+
+		if (LanguageManager.Instance.IsLanguageSupportedEnglishName(systemLanguage))
+		{
+			strLanguageCode = returnLanguageCode();
+		}
+		else
+		{
+			strLanguageCode = "en";
+		}
+		LanguageManager.Instance.ChangeLanguage(strLanguageCode);
+	}
+}
---
+++ Assets/Prototyping Game/Scripts/Utils/TranslateSmartLocalization.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Globalization;
+using SmartLocalization;
+
+public class TranslateSmartLocalization : MonoBehaviour {
+
+	LanguageManager mLanguageManager;
+		
+	void Start()
+	{
+		mLanguageManager = LanguageManager.Instance;
+		TextTranslate();
+	}
+	
+	public void TextTranslate()
+	{
+		string str = mLanguageManager.GetTextValue(this.name);
+		if (!string.IsNullOrEmpty(str))
+		{
+			GetComponent<Text>().text = str;
+		}
+		else
+		{
+			Debug.LogWarning("A string " + this.name + " não possui uma traducao configurada");
+		}
+	}
+}
