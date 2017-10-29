7e1d24ec35750144db9e839cfbd78deb879612d6
Willian Gomes de Oliveira <willian.goliveira@outlook.com>
2017-10-29 17:15:35 -0200

    Colocando o DoozyUI

--- Assets/AdDuplexInterop.cs
+++ Assets/AdDuplexInterop.cs
@@ -2,79 +2,82 @@
 using System.Collections;
 using System;
 
-public static class AdDuplexInterop
+namespace CountingSheeps.NewOldPuzzle
 {
-    /// <summary>
-    /// 
-    /// </summary>
-    public static event EventHandler LoadInterstitialEvent;
+	public static class AdDuplexInterop
+	{
+		/// <summary>
+		/// 
+		/// </summary>
+		public static event EventHandler LoadInterstitialEvent;
 
-    /// <summary>
-    /// 
-    /// </summary>
-    public static event EventHandler ShowInterstitialEvent;
+		/// <summary>
+		/// 
+		/// </summary>
+		public static event EventHandler ShowInterstitialEvent;
 
-    /// <summary>
-    /// 
-    /// </summary>
-    public static event EventHandler AdControlShow;
+		/// <summary>
+		/// 
+		/// </summary>
+		public static event EventHandler AdControlShow;
 
-    /// <summary>
-    /// 
-    /// </summary>
-    public static event EventHandler AdControlHide;
+		/// <summary>
+		/// 
+		/// </summary>
+		public static event EventHandler AdControlHide;
 
-    public static event EventHandler OnInterstitialClosed;
+		public static event EventHandler OnInterstitialClosed;
 
-    /// <summary>
-    /// 
-    /// </summary>
-    public static void ShowAdControl()
-    {
-        if (AdControlShow != null)
-        {
-            AdControlShow(null, EventArgs.Empty);
-        }
-    }
+		/// <summary>
+		/// 
+		/// </summary>
+		public static void ShowAdControl()
+		{
+			if (AdControlShow != null)
+			{
+				AdControlShow(null, EventArgs.Empty);
+			}
+		}
 
-    /// <summary>
-    /// 
-    /// </summary>
-    public static void HideAdControl()
-    {
-        if (AdControlHide != null)
-        {
-            AdControlHide(null, EventArgs.Empty);
-        }
-    }
+		/// <summary>
+		/// 
+		/// </summary>
+		public static void HideAdControl()
+		{
+			if (AdControlHide != null)
+			{
+				AdControlHide(null, EventArgs.Empty);
+			}
+		}
 
-    /// <summary>
-    /// 
-    /// </summary>
-    public static void LoadInterstitialAd()
-    {
-        if (LoadInterstitialEvent != null)
-        {
-            LoadInterstitialEvent(null, EventArgs.Empty);
-        }
-    }
+		/// <summary>
+		/// 
+		/// </summary>
+		public static void LoadInterstitialAd()
+		{
+			if (LoadInterstitialEvent != null)
+			{
+				LoadInterstitialEvent(null, EventArgs.Empty);
+			}
+		}
 
-    /// <summary>
-    /// 
-    /// </summary>
-    public static void ShowInterstitialAd()
-    {
-        if (ShowInterstitialEvent != null)
-        {
-            ShowInterstitialEvent(null, EventArgs.Empty);
-        }
-    }
+		/// <summary>
+		/// 
+		/// </summary>
+		public static void ShowInterstitialAd()
+		{
+			if (ShowInterstitialEvent != null)
+			{
+				ShowInterstitialEvent(null, EventArgs.Empty);
+			}
+		}
 
-    public static void InterstitialClose()
-    {
-        if (OnInterstitialClosed != null)
-        {
-            OnInterstitialClosed(null, EventArgs.Empty);
-        }
-    }
+		public static void InterstitialClose()
+		{
+			if (OnInterstitialClosed != null)
+			{
+				OnInterstitialClosed(null, EventArgs.Empty);
+			}
+		}
+	}
 }
